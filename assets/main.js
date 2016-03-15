b54e4fe42a06bf89f4af83c64a2af5697e07213c
dobro-731 <gvas94@ya.ru>
2016-03-15 19:40:01 +0000

    js

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,25 @@
-alert("hello")
+var string = "hello";
+var number = 1;
+var nl = null;
+var undf = undefined;
+var boo = true;
+var user = {
+    name: "john"
+};
+var func = function(){};
+
+console.log(typeof (string));
+console.log(typeof (number));
+console.log(typeof (nl));
+console.log(typeof (undf));
+console.log(typeof (boo));
+console.log(typeof (user));
+console.log(typeof (func));
+
+var result = prompt("Ты серьезно", "да");
+var isAdmin = confirm("вы адинистратор?");
+
+//логические опер !=(не равно) ==(равно) ||(или) &&(и) !(отрицание)   
+if (isAdmin != true) {
+    
+}
